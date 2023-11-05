57cd970e21165560731bf16126bba204a0ee3bf5
mdnazmul42726 <nazmulofficial@outlook.com>
2023-11-05 13:17:12 +0600

    Book Now section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -141,7 +141,7 @@ const Home = () => {
             </div>
             {/* our top selling foods card */}
             <div className="my-36">
-                <h1 className='text-3xl font-bold text-center'>Pop Selling Foods</h1>
+                <h1 className='text-4xl font-bold text-center'>Pop Selling Foods</h1>
                 <div className="grid grid-cols-1 md:grid-cols-3">
                     <div className="mx-auto mt-11 w-80 transform overflow-hidden rounded-md shadow-md duration-300 hover:scale-105 hover:shadow-lg">
                         <img className="h-48 w-full object-cover object-center" src="https://i.ibb.co/PZZrXd9/salmon-zucchini.jpg" alt="Product Image" />
@@ -169,7 +169,7 @@ const Home = () => {
                                 Meet the Team
                             </h1>
                             <p className="text-gray-700 text-lg font-light">
-                                With over 100 years of combined experience, we've got a well-seasoned team at the helm.
+                                With over 100 years of combined experience, we ve got a well-seasoned team at the helm.
                             </p>
                         </div>
                     </div>
@@ -295,7 +295,147 @@ const Home = () => {
                 </div>
             </div>
             {/* 1 */}
+            {/* <!-- component -->
+            <style>@import url('https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css')</style> */}
+            <div className="min-w-screen min-h-screen bg-gray-50 flex items-center justify-center">
+                <div className="w-full bg-white border-t border-b border-gray-200 px-5 py-16 md:py-24 text-gray-800">
+                    <div className="w-full max-w-6xl mx-auto">
+                        <div className="text-center max-w-xl mx-auto">
+                            <h1 className="text-6xl md:text-7xl font-bold mb-5 text-gray-600">What people <br />are saying.</h1>
+                            <h3 className="text-xl mb-5 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                            <div className="text-center mb-10">
+                                <span className="inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                                <span className="inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                                <span className="inline-block w-40 h-1 rounded-full bg-indigo-500"></span>
+                                <span className="inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                                <span className="inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                            </div>
+                        </div>
+                        <div className="-mx-3 md:flex items-start">
+                            <div className="px-3 md:w-1/3">
+                                <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                                    <div className="w-full flex mb-4 items-center">
+                                        <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                            <img src="https://i.pravatar.cc/100?img=1" alt="" />
+                                        </div>
+                                        <div className="flex-grow pl-3">
+                                            <h6 className="font-bold text-sm uppercase text-gray-600">Kenzie Edgar.</h6>
+                                        </div>
+                                    </div>
+                                    <div className="w-full">
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1"></span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sunt ratione dolor exercitationem minima quas itaque saepe quasi architecto vel! Accusantium, vero sint recusandae cum tempora nemo commodi soluta deleniti.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1"></span></p>
+                                    </div>
+                                </div>
+                                <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                                    <div className="w-full flex mb-4 items-center">
+                                        <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                            <img src="https://i.pravatar.cc/100?img=2" alt="" />
+                                        </div>
+                                        <div className="flex-grow pl-3">
+                                            <h6 className="font-bold text-sm uppercase text-gray-600">Stevie Tifft.</h6>
+                                        </div>
+                                    </div>
+                                    <div className="w-full">
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1"></span>Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Dolore quod necessitatibus, labore sapiente, est, dignissimos ullam error ipsam sint quam tempora vel.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1"></span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="px-3 md:w-1/3">
+                                <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                                    <div className="w-full flex mb-4 items-center">
+                                        <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                            <img src="https://i.pravatar.cc/100?img=3" alt="" />
+                                        </div>
+                                        <div className="flex-grow pl-3">
+                                            <h6 className="font-bold text-sm uppercase text-gray-600">Tommie Ewart.</h6>
+                                        </div>
+                                    </div>
+                                    <div className="w-full">
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, obcaecati ullam excepturi dicta error deleniti sequi.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1"></span></p>
+                                    </div>
+                                </div>
+                                <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                                    <div className="w-full flex mb-4 items-center">
+                                        <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                            <img src="https://i.pravatar.cc/100?img=4" alt="" />
+                                        </div>
+                                        <div className="flex-grow pl-3">
+                                            <h6 className="font-bold text-sm uppercase text-gray-600">Charlie Howse.</h6>
+                                        </div>
+                                    </div>
+                                    <div className="w-full">
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1"></span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto inventore voluptatum nostrum atque, corrupti, vitae esse id accusamus dignissimos neque reprehenderit natus, hic sequi itaque dicta nisi voluptatem! Culpa, iusto.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1"></span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="px-3 md:w-1/3">
+                                <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                                    <div className="w-full flex mb-4 items-center">
+                                        <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                            <img src="https://i.pravatar.cc/100?img=5" alt="" />
+                                        </div>
+                                        <div className="flex-grow pl-3">
+                                            <h6 className="font-bold text-sm uppercase text-gray-600">Nevada Herbertson.</h6>
+                                        </div>
+                                    </div>
+                                    <div className="w-full">
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1"></span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptatem porro obcaecati dicta, quibusdam sunt ipsum, laboriosam nostrum facere exercitationem pariatur deserunt tempora molestiae assumenda nesciunt alias eius? Illo, autem!<span className="text-lg leading-none italic font-bold text-gray-400 ml-1"></span></p>
+                                    </div>
+                                </div>
+                                <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                                    <div className="w-full flex mb-4 items-center">
+                                        <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                            <img src="https://i.pravatar.cc/100?img=6" alt="" />
+                                        </div>
+                                        <div className="flex-grow pl-3">
+                                            <h6 className="font-bold text-sm uppercase text-gray-600">Kris Stanton.</h6>
+                                        </div>
+                                    </div>
+                                    <div className="w-full">
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1"></span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto, explicabo, cupiditate quas totam!<span className="text-lg leading-none italic font-bold text-gray-400 ml-1"></span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+                <div>
+                    <a title="Buy me a beer" href="https://www.buymeacoffee.com/mdnazmul42726" className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+                        <img className="object-cover object-center w-full h-full rounded-full" src="https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg" alt="Buy me a beer" />
+                    </a>
+                </div>
+            </div>
+            {/* Book */}
+
+            <div className="bg-[url('https://i.ibb.co/0J5pyT5/reservation-bg.jpg')] mb-96 md:min-h-screen">
+                <div className="w-full md:w-96 h-[500px] bg-white md:ml-10 ">
+                    <p className='text-red-500 text-center pt-10 font-mono'>Reservation</p>
+                    <h1 className='text-3xl text-center font-sans'>Book Now</h1>
+                    <form className='p-10'>
+                        <input className='p-2 w-full border rounded-sm mb-3' type="text" name="" id="" placeholder='Name' required />
+                        <input className='p-2 w-full border rounded-sm mb-3' type="text" name="" id="" placeholder='Email' required/>
+                        <input className='p-2 w-full border rounded-sm' type="text" name="" id="" placeholder='Phone' required/>
+                        <div className="flex mt-3 gap-3">
+                            <input className='p-2 w-full border rounded-sm' type="date" name="" id="" placeholder='Date' required/>
+                            <input className='p-2 w-full border rounded-sm' type="time" name="" id="" placeholder='Time' required/>
+                        </div>
+                        <select className='p-2 w-full border rounded-sm mt-3' name="" id="" required>
+                            <option value="N/A">Person</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">5</option>
+                            <option value="6">6</option>
+                        </select>
+                        <div className="flex justify-center">
+                        <button type='submit' className="btn mt-5 ml-3 bg-[#F34949] px-7 hover:bg-[#F34949] hover:scale-105 text-white rounded-full">Book A Table</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
