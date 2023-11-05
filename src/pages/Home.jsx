eeb7ef0d8b7b80d57dd5db6629ce88ae45b18401
mdnazmul42726 <nazmulofficial@outlook.com>
2023-11-05 12:01:05 +0600

    Meet the Team section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import { BsGooglePlay, BsDoorOpen } from 'react-icons/bs';
-import { AiFillApple } from 'react-icons/ai';
+import { AiFillApple, AiFillLinkedin } from 'react-icons/ai';
 import { GoRocket } from 'react-icons/go';
 import { BiLeaf } from 'react-icons/bi';
+import { BsFacebook } from 'react-icons/bs';
+import { RiTwitterXFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
+// import { FaXTwitter } from 'react-icons/fa';
 
 const Home = () => {
 
@@ -120,7 +124,7 @@ const Home = () => {
                     <div className="flex flex-col md:flex-row justify-between w-11/12 mx-auto">
 
                         <div className="">
-                            <img src="https://i.ibb.co/MGCNLn8/steak.jpg" alt="" />
+                            <img src="https://i.ibb.co/PZZrXd9/salmon-zucchini.jpg" alt="" />
                         </div>
                         <div className="mt-10 md:mt-0">
                             <div className="flex"><h1 className='text-5xl font-bold'>02.</h1><p className='mt-5 ml-2 font-bold'>________________</p></div>
@@ -135,6 +139,163 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            {/* our top selling foods card */}
+            <div className="my-36">
+                <h1 className='text-3xl font-bold text-center'>Pop Selling Foods</h1>
+                <div className="grid grid-cols-1 md:grid-cols-3">
+                    <div className="mx-auto mt-11 w-80 transform overflow-hidden rounded-md shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+                        <img className="h-48 w-full object-cover object-center" src="https://i.ibb.co/PZZrXd9/salmon-zucchini.jpg" alt="Product Image" />
+                        <div className="p-4">
+                            <h2 className="mb-2 text-lg font-medium">Product Name</h2>
+                            <p className="text-base"><span className='font-semibold'>Category:</span> xxx</p>
+                            <p className=" text-base"><span className='font-semibold'>Orders:</span> 001</p>
+                            <div className="flex items-center">
+                                <p className="mr-2 text-2xl font-semibold">$20.00</p>
+                                <button className='ml-auto text-base font-medium bg-red-500 hover:bg-red-600 px-4 rounded-sm text-white py-2'>Details</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <button className=' text-base font-medium bg-red-500 hover:bg-red-600 px-4 rounded-sm text-white py-2'>See All</button>
+                </div>
+            </div>
+            {/* meet the tram */}
+            <div className="  ">
+                <div className="container max-w-7xl px-4">
+                    <div className="flex flex-wrap justify-center text-center mb-24">
+                        <div className="w-full lg:w-6/12 px-4">
+                            <h1 className="text-gray-900 text-4xl font-bold mb-8">
+                                Meet the Team
+                            </h1>
+                            <p className="text-gray-700 text-lg font-light">
+                                With over 100 years of combined experience, we've got a well-seasoned team at the helm.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap">
+                        <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                            <div className="flex flex-col">
+                                <a href="#" className="mx-auto">
+                                    <img className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                        src="https://i.ibb.co/tMmZvyv/chef-1.jpg" />
+                                </a>
+                                <div className="text-center mt-6">
+                                    <h1 className="text-gray-900 text-xl font-bold mb-1">
+                                        Aaron Patel
+                                    </h1>
+                                    <div className="text-gray-700 font-light mb-2">
+                                        Founder CEO
+                                    </div>
+                                    <div className="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+
+                                        <Link className="flex rounded-full h-20 w-8">
+                                            <i className="mdi mdi-linkedin text-indigo-500 mx-auto mt-2"><BsFacebook className='text-xl' /></i> </Link>
+
+                                        <Link className="flex rounded-full h-20 w-8">
+                                            <i className="mdi mdi-twitter text-blue-300 mx-auto mt-2"><AiFillLinkedin className='text-xl' /></i>
+                                        </Link>
+                                        <Link className="flex rounded-full  h-20 w-8">
+                                            <i className="mdi mdi-instagram  mx-auto mt-2 text-black"><RiTwitterXFill className='text-xl' /></i>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                            <div className="flex flex-col">
+
+                                <a href="#" className="mx-auto">
+                                    <img className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                        src="https://i.ibb.co/xYs5Khv/chef-2.jpg" />
+                                </a>
+                                <div className="text-center mt-6">
+
+                                    <h1 className="text-gray-900 text-xl font-bold mb-1">
+                                        Daniel Tebas
+                                    </h1>
+                                    <div className="text-gray-700 font-light mb-2">
+                                        Chef
+                                    </div>
+                                    <div className="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+
+                                        <Link className="flex rounded-full h-20 w-8">
+                                            <i className="mdi mdi-linkedin text-indigo-500 mx-auto mt-2"><BsFacebook className='text-xl' /></i> </Link>
+
+                                        <Link className="flex rounded-full h-20 w-8">
+                                            <i className="mdi mdi-twitter text-blue-300 mx-auto mt-2"><AiFillLinkedin className='text-xl' /></i>
+                                        </Link>
+                                        <Link className="flex rounded-full  h-20 w-8">
+                                            <i className="mdi mdi-instagram  mx-auto mt-2 text-black"><RiTwitterXFill className='text-xl' /></i>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                            <div className="flex flex-col">
+
+                                <a href="#" className="mx-auto">
+                                    <img className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                        src="https://i.ibb.co/VvKJkqK/pexels-2102934.jpg" />
+                                </a>
+                                <div className="text-center mt-6">
+                                    <h1 className="text-gray-900 text-xl font-bold mb-1">
+                                        Jon Snow
+                                    </h1>
+                                    <div className="text-gray-700 font-light mb-2">
+                                        Chef
+                                    </div>
+                                    <div className="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+                                        <Link className="flex rounded-full h-20 w-8">
+                                            <i className="mdi mdi-linkedin text-indigo-500 mx-auto mt-2"><BsFacebook className='text-xl' /></i> </Link>
+
+                                        <Link className="flex rounded-full h-20 w-8">
+                                            <i className="mdi mdi-twitter text-blue-300 mx-auto mt-2"><AiFillLinkedin className='text-xl' /></i>
+                                        </Link>
+                                        <Link className="flex rounded-full  h-20 w-8">
+                                            <i className="mdi mdi-instagram  mx-auto mt-2 text-black"><RiTwitterXFill className='text-xl' /></i>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                            <div className="flex flex-col">
+                                <a href="#" className="mx-auto">
+                                    <img className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                        src="https://i.ibb.co/0yFkH24/chef-3.jpg" />
+                                </a>
+                                <div className="text-center mt-6">
+                                    <h1 className="text-gray-900 text-xl font-bold mb-1">
+                                        Patel Ex
+                                    </h1>
+                                    <div className="text-gray-700 font-light mb-2">
+                                        Chef
+                                    </div>
+                                    <div className="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+                                        <Link className="flex rounded-full h-20 w-8">
+                                            <i className="mdi mdi-linkedin text-indigo-500 mx-auto mt-2"><BsFacebook className='text-xl' /></i> </Link>
+
+                                        <Link className="flex rounded-full h-20 w-8">
+                                            <i className="mdi mdi-twitter text-blue-300 mx-auto mt-2"><AiFillLinkedin className='text-xl' /></i>
+                                        </Link>
+                                        <Link className="flex rounded-full  h-20 w-8">
+                                            <i className="mdi mdi-instagram  mx-auto mt-2 text-black"><RiTwitterXFill className='text-xl' /></i>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* 1 */}
+
         </div>
     );
 };
